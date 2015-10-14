4a85b2078f577537b59db50085132e926991163a
Florida Elago <florida.elago@gmail.com>
2015-10-13 19:47:55 -0700

    initial commit

---
+++ spec/scraper_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Scraper do
+  it 'has a version number' do
+    expect(Scraper::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
