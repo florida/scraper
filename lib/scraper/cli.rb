952dd563dd94900678e4275254d82aaa54c3521e
Florida Elago <florida.elago@gmail.com>
2015-10-27 22:29:27 -0700

    update parameters and options

--- lib/scraper/cli.rb
+++ lib/scraper/cli.rb
@@ -1,16 +1,20 @@
 require 'thor'
 require 'scraper/crawler'
+require 'scraper/helpers/logging'
 
 module Scraper
   class CLI < Thor
-    option :domain
+    include Scraper::Logging
+
     option :ignore_list
 
-    desc "url", "scrape url"
-    def scrape
-      crawler = Scraper::Crawler.new(options[:domain])
+    desc "scrape [DOMAIN]", "Scrapes the url of a given [DOMAIN] and outputs to a json file"
+    def scrape(domain)
+      logger.info(set_color "scraping #{domain}", :yellow)
+      crawler = Scraper::Crawler.new(domain)
       crawler.do_crawl
-      puts "scraping #{options[:domain]}"
+    rescue SocketError
+      logger.error(set_color "Internet connection not found, please check internet connection or try again later.", :red)
     end
   end
 end
