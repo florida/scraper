07c031a2f0c7f9da2a7f2bd656bb507219e01e9d
Florida Elago <florida.elago@gmail.com>
2015-10-29 21:18:33 -0700

    refactor logging

--- lib/scraper/helpers/logging.rb
+++ lib/scraper/helpers/logging.rb
@@ -2,14 +2,19 @@ module Scraper
   module Logging
     def logger
       create_log_file unless @logger
-      output = ENV['ENVIRONMENT'] === 'test' ? 'log/test.log' : STDOUT
-      @logger ||= Logger.new(output)
+      @logger ||= Logger.new(output_destination)
       @logger.level = Logger::INFO
       @logger
     end
 
     def create_log_file
       Dir.mkdir('log') if !File.directory?('log') && ENV['ENVIRONMENT'] === 'test'
+    end
+
+    protected
+
+    def output_destination
+      ENV['ENVIRONMENT'] === 'test' ? 'log/test.log' : STDOUT
     end
   end
 end
