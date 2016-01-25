5af763ebbefe02fc768a60e7c487e8fb0e756dc0
florida <florida.elago@gmail.com>
2016-01-25 10:07:00 -0800

    remove ignore lists

--- lib/scraper/crawler.rb
+++ lib/scraper/crawler.rb
@@ -6,8 +6,6 @@ module Scraper
     include Scraper::Logging
     attr_accessor :domain
 
-    IGNORE_LIST = /community|blog/
-
     def initialize(domain, options = {})
       @domain = domain
       @ignore_list = options[:ignore_list] || IGNORE_LIST
@@ -16,14 +14,17 @@ module Scraper
 
     def do_crawl
       pages = crawl(@domain, {})
-      File.open(@output_file, 'w') do |f|
-        f.write pages.to_json
+
+      begin
+        File.open(@output_file, 'w') do |f|
+          f.write pages.to_json
+        end
+      rescue Errno::EACCES => e
+        logger.error(e.message)
       end
     end
 
     def crawl(domain, data = {})
-      return if /community|help|one-click-apps|blog/ =~ domain
-
       content = Scraper::LinkContentParser.new(domain)
       pages = content.get_all_links
       assets = content.get_all_assets
